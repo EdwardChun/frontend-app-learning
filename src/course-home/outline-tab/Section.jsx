eabb8f867d2c055e8482626d922accac482b32cb
EdwardChun <47728996+EdwardChun@users.noreply.github.com>
2024-02-26 11:36:34 +0800

    Add files via upload
    update section to have topic thumbnail

--- src/course-home/outline-tab/Section.jsx
+++ src/course-home/outline-tab/Section.jsx
@@ -27,6 +27,7 @@ const Section = ({
   const {
     courseBlocks: {
       sequences,
+      sections,
     },
   } = useModel('outline', courseId);
 
@@ -38,8 +39,23 @@ const Section = ({
 
   useEffect(() => {
     setOpen(defaultOpen);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  function topicThumbnail() {
+
+    let asset = courseId.replace("course-v1:", "asset-v1:")
+    let imageName = title.toLowerCase();
+    imageName = "tb_" + imageName.replace(/[\(\)& -]/g, "_");
+
+    let domain = window.location.origin;
+    domain = domain.replace("app.", "");
+    domain = domain.replace(":2000", ":8000");
+
+    let thumbnailUrl = domain + "/" + asset + "+type@asset+block@" + imageName + ".png";
+
+    return <img src={thumbnailUrl} style={{ width: "50%", paddingRight: "15px", paddingBottom: "10px" }} />;
+  }
 
   const sectionTitle = (
     <div className="row w-100 m-0">
@@ -63,6 +79,7 @@ const Section = ({
         )}
       </div>
       <div className="col-10 ml-3 p-0 font-weight-bold text-dark-500">
+        <div>{topicThumbnail()}</div>
         <span className="align-middle">{title}</span>
         <span className="sr-only">
           , {intl.formatMessage(complete ? messages.completedSection : messages.incompleteSection)}
